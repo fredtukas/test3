4cec2c6dcd6900a9af6dabd08fd09b5abba77976
fredtukas <fredtukas@yahoo.fr>
2019-09-30 15:49:46 +0400

    boucles1

---
+++ www/boucles1/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$var = 0;
+while ($var <=10)
+{
+    echo "ceci est la ligne n° . $var . '<br />'";
+    $var++;
+}
+    ?>
+</body>
+</html>
