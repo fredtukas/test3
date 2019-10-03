5f50728bcb80ceb4eecce605bd57b200b21ff304
titoff974 <fredtukas@yahoo.fr>
2019-10-03 13:30:58 +0400

    accueil

---
+++ www/maquette/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">
+<p>Copyright inc. 2019</p>
+</div>
---
+++ www/test7/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>variables</title>
+</head>
+<body>
+    <?php
+$lastname = " Fred";
+$firstname = " Pech";
+$age = 42;
+echo "Bonjour ";
+echo $lastname;
+echo $firstname;
+echo " tu as ";
+echo $age;
+echo " ans";
+
+
+    ?>
+</body>
+</html>
