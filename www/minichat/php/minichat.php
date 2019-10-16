cf5dfb3fe5051925f8b1ffb46343cf9e4f3cbbee
fredtukas <fredtukas@yahoo.fr>
2019-10-16 14:20:04 +0400

    termine

--- www/minichat/php/minichat.php
+++ www/minichat/php/minichat.php
@@ -1,50 +1,31 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mini-chat</title>
-    </head>
-    <style>
-    form
-    {
-        text-align:center;
-    }
-    </style>
-    <body>
-    
-    <form action="minichat_post.php" method="post">
-        <p>
-        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
-        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
-
-        <input type="submit" value="Envoyer" />
-	</p>
-    </form>
+<form action="minichat_post.php" method="post">
+    <label for="nom">Votre nom :</label>
+    <input type="text" name="nom" id="nom">
+    <label for="message">Votre message :</label>
+    <input type="text" name="message" id="message">
+    <input type="submit">
+</form>
 
 <?php
-// Connexion à la base de données
+
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=minichat2;charset=utf8', 'root', '');
 }
 catch(Exception $e)
 {
         die('Erreur : '.$e->getMessage());
 }
 
+$reponse = $bdd->query('SELECT nom, message FROM message');
 
-
-// Récupération des 10 derniers messages
-$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
-
-// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
 while ($donnees = $reponse->fetch())
 {
-	echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+?>
+    <p><?php echo $donnees['nom']?></p>
+    <p><?php echo $donnees['message']?></p>
+<?php
 }
 
-$reponse->closeCursor();
-
+$reponse->closeCursor()
 ?>
-    </body>
-</html>
--- www/minichat/php/minichat_post.php
+++ www/minichat/php/minichat_post.php
@@ -1,5 +1,21 @@
 <?php
-//Effectuer ici la requete qui insere le message
-//Puis rediriger vers minichat.php comme ceci:
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=minichat2;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+
+$nom = $_POST['nom'];
+$message = $_POST['message'];
+
+$req = $bdd->prepare('INSERT INTO message(nom, message) VALUES(:nom, :message)');
+$req->execute(array(
+    'nom' => $nom,
+    'message' => $message
+));
+
 header('Location: minichat.php');
-?>
+
